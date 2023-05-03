ec3b1be9f33105f8b994fdf283561b47a3ed1388
krzyczak <krzyczak@users.noreply.github.com>
2023-05-03 18:47:03 +0100

    chore: add actix-web and chrone and first /health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::{DateTime, Utc};
+
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    let now: DateTime<Utc> = Utc::now();
+    format!("Current time is {}!", now)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
